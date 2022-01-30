15281b299d89584d015f55d95a21c021d31c9c21
Mark <andrei.marcu35@gmail.com>
2022-01-30 18:38:24 +0200

    Initial commit

---
+++ src/main/java/com/anmarcu/AudioContractApplication.java
@@ -0,0 +1,13 @@
+package com.anmarcu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudioContractApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AudioContractApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/anmarcu/AudioContractApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anmarcu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudioContractApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
